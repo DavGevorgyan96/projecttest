994efa68fe19dfcf2c19d94fc30b2fc549bc7918
DavGevorgyan96 <gevorgyandav96@gmail.com>
2022-09-06 16:26:12 +0400

    Adddd

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import Header from "./pages/header";
 import Category from './pages/navbarCategory';
 
 function App() {
+  const navCategory = [
+    {name: "ВЕСТЕРН"},
+    {name: "БИОГРАФИЯ"},
+    {name: "БОЕВИК"},
+    {name: "ВОЕННЫЙ"},
+    {name: "ДЕТЕКТИВ"},
+    {name: "ДРАМА"},
+    {name: "ДОКУМЕНТАЛЬНЫЙ"},
+    {name: "ИСТОРИЯ"},
+    {name: "КОМЕДИЯ"},
+    {name: "КРИМИНАЛ"},
+    {name: "МЕЛОДРАМА"},
+    {name: "МУЗЫКА"},
+    {name: "МУЛЬТФИЛЬМ"},
+    {name: "ПРИКЛЮЧЕНИЯ"},
+    {name: "СЕМЕЙНЫЙ"},
+    {name: "СЕРИАЛЫ"},
+    {name: "СПОРТ"},
+    {name: "ТРИЛЛЕР"},
+    {name: "УЖАСЫ"},
+    {name: "ФЭНТЕЗИ"},
+    {name: "ФАНТАСТИКА"}
+  ]
   return (
     <div className="App">
       <Header />
-      <Category />
+      <Category navCategory={navCategory}/>
     </div>
   );
 }
